cfb1951fb9d8bc58c44d4a6b9e8d04ad0c0a326a
Yash Bodhe <106073201+Yash12007@users.noreply.github.com>
2023-12-07 14:05:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, request, json, jsonify, url_for
+
+app = Flask(__name__)
+    
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/doctor')
+def doctor():
+    return render_template('doctor.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if (request.method == 'POST'):
+        name = request.form.get('name')
+        address = request.form.get('address')
+        number = request.form.get('number')
+        dateandtime = request.form.get('datetime')
+        lead = (name, number, address, dateandtime)
+    return ''
+
+if __name__=='__main__':
+    app.run(debug=True)
